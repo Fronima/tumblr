285b46f2123681d887e5fbdcb604bceaf56b4f9b
Fronima <frankcoleman55@gmail.com>
2018-02-04 20:18:27 -0500

    added READ.md 	.gitignore 	Podfile 	Podfile.lock 	Pods/

---
+++ tumblr/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  tumblr
+//
+//  Created by ARG Lab on 1/31/18.
+//  Copyright © 2018 ARG Lab. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
